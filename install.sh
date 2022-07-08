4e7260ec8d59bd6a533bf148540b181515dc5562
ge32sat <ge32sat@mytum.de>
2022-07-08 16:03:33 +0200

    add install script

---
+++ install.sh
@@ -0,0 +1,18 @@
+read -p "This script will delete your existing configuration files. This includes sway, nvim, waybar and alacritty. Would you like to continue? (y/N)?" CONT
+if [ "$CONT" = "y" ]; then
+    echo "Deleting existing files...";
+
+    rm -r ~/.config/sway;
+    rm -r ~/.config/alacritty;
+    rm -r ~/.config/waybar;
+    rm -r ~/.config/nvim;
+
+    ln -s ${PWD}/sway ~/.config/sway;
+    ln -s ${PWD}/alacritty ~/.config/alacritty;
+    ln -s ${PWD}/waybar ~/.config/waybar;
+    ln -s ${PWD}/nvim ~/.config/nvim;
+
+    echo "Existing files deleted.";
+else
+    echo "Cancelled.";
+fi
