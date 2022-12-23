5f974576290d62f916e448040b50101305697896
Fabian Specht <fabiancodes@gmail.com>
2022-12-23 12:25:49 +0100

    pass dmenu wayland integration

--- scripts/otpmenu.sh
+++ scripts/otpmenu.sh
@@ -1,12 +1,15 @@
-#!/usr/bin/env bash
+#!/bin/sh
+
+dmenu=dmenu-wl
+xdotool="ydotool type --file -"
 
 export PASSWORD_STORE_DIR="$HOME/.password-store"
 cd $PASSWORD_STORE_DIR
 
-PASS=`find * -name '*.gpg' | sed s/\.gpg// | dmenu -l 10`
+PASS=`find * -name '*.gpg' | sed s/\.gpg// | "$dmenu" -l 10`
 [[ -n $PASS ]] || exit
 
 PW=`pass otp $PASS | { read -r pass; printf %s "$pass"; }`
 
-xdotool type --clearmodifiers --delay 0 $PW
 notify-send $PW --expire-time=20000
+wl-copy $PW
