5fa5b096ab1a656e45c9a3e91b3f4374eca6363a
Fabian Specht <fabiancodes@gmail.com>
2023-04-17 00:17:27 +0200

    pywal i3 & polybar integration

--- nvim/lua/user/mappings.lua
+++ nvim/lua/user/mappings.lua
@@ -22,7 +22,7 @@ vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("n", "<F4>", ":set hlsearch!<CR>", opts)
 
 vim.keymap.set("n", "<leader>f", ":CtrlP .<CR>")
-vim.keymap.set("n", "<leader>g", ":Ack ")
+vim.keymap.set("n", "<leader>g", ":Ag ")
 vim.keymap.set("n", "<leader>e", ":Ex<CR>")
 
 
